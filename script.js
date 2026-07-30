5f30b9b510fdcb11754a2c4edeea401fa8bf1230
chessumbrian9-web <chessumbrian9@gmail.com>
2026-07-30 22:50:46 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+function topFunction() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
