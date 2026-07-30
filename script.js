f15023a0d4df3f1f4e412b53c8a2e5be230944b7
chessumbrian9-web <chessumbrian9@gmail.com>
2026-07-31 01:14:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-let topBtn = document.getElementById("topBtn");
+const slides = document.querySelectorAll(".slide");
 
-window.onscroll = function () {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        topBtn.style.display = "block";
-    } else {
-        topBtn.style.display = "none";
+let current = 0;
+
+function nextSlide(){
+
+    slides[current].classList.remove("active");
+
+    current++;
+
+    if(current >= slides.length){
+        current = 0;
     }
-};
 
-function topFunction() {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
+    slides[current].classList.add("active");
+}
+
+setInterval(nextSlide,5000);
+
+// Back to top button
+let topBtn=document.getElementById("topBtn");
+
+window.onscroll=function(){
+
+if(document.body.scrollTop>300 || document.documentElement.scrollTop>300){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+}
+
+function topFunction(){
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
 }
